201ccce5a9d3e53e9cb2b88a49c6e1c9ab86e315
SuntaeKim <stkim@jbnu.ac.kr>
2017-09-01 09:59:14 +0900

    initial commit

--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit2.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit2.java
@@ -1,6 +1,6 @@
 package kr.ac.jbnu.se.advweb.gitplay0;
 
-public class HelloGit {
+public class HelloGit2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit3.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit3.java
@@ -1,6 +1,6 @@
 package kr.ac.jbnu.se.advweb.gitplay0;
 
-public class HelloGit {
+public class HelloGit3 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
