8d4942840a022b68367a6ab18158c1d796c4e678
jipsin08 <31507007+jipsin08@users.noreply.github.com>
2017-09-01 10:05:42 +0900

    Update HelloGit.java
    code update test

--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
-		
 		// how are you doing??
+		System.out.println("Hello Git!!???????????");
 	}
 }
